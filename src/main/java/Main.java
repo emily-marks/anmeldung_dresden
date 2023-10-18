b8ae30aea98d2a82369ca40f5332ba59f536a719
Emily Marx <emilyhufflish@gmail.com>
2023-10-18 07:59:33 +0200

    extend for fuererscheinbehoerde

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,18 +7,19 @@ import utils.AllBueros;
 import utils.Month;
 
 import java.io.BufferedReader;
+import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.nio.charset.StandardCharsets;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
-import java.util.Comparator;
 import java.util.HashMap;
 import java.util.Map;
 
 import static java.lang.Integer.parseInt;
-import static java.util.Map.Entry.*;
+import static java.util.Map.Entry.comparingByValue;
 import static utils.Constants.*;
 import static utils.EmailUtils.sendToEmail;
 
@@ -27,29 +28,50 @@ public class Main {
     public static LocalDate nearestDate;
     public static Map<String, LocalDate> batchAppointments = new HashMap<>();
     public static String officeName;
-    public static final LocalDate CURRENT_BOOKING = LocalDate.of(2022, java.time.Month.OCTOBER, 17);
+    public static final LocalDate CURRENT_BOOKING = LocalDate.of(2023, java.time.Month.DECEMBER, 15);
     private final static Logger logger = Logger.getLogger(Main.class);
 
     public static void main(String[] args) throws IOException, InterruptedException {
+//        bürger_büro();
+        führerscheinbehörde();
+    }
+
+    private static void führerscheinbehörde() throws InterruptedException, IOException {
+        while (true) {
+            nearestDate = checkMostRecentDate();
+
+            if (nearestDate.compareTo(CURRENT_BOOKING) < 0) {
+                String message = "You can book " + nearestDate.format(DateTimeFormatter.ofPattern("dd.MM.yy"));
+                sendToEmail(message);
+                logger.info(message);
+                break;
+            } else {
+                logger.info("Last date available: " + nearestDate);
+            }
+            Thread.sleep(600000);
+        }
+    }
+
+    private static void bürger_büro() throws IOException, InterruptedException {
         while (true) {
             for (int i = 0; i <= 8; i++) checkMostRecentDateByOffice(i);
 
             if (nearestDate.compareTo(CURRENT_BOOKING) < 0) {
                 String message = "You can book " + nearestDate.format(DateTimeFormatter.ofPattern("dd.MM.yy")) + " in " + officeName + " now";
-//                sendToEmail(message);
+                sendToEmail(message);
                 logger.info(message);
                 break;
             } else {
                 printSorted(batchAppointments);
                 logger.info("No available appointments.");
             }
-            Thread.sleep(600000/5);
+            Thread.sleep(600000);
         }
     }
 
     private static void checkMostRecentDateByOffice(int officeId) throws IOException {
         BufferedReader in = new BufferedReader(
-                new InputStreamReader(getConnection(officeId).getInputStream()));
+                new InputStreamReader( getConnection(officeId).getInputStream()));
         String inputLine;
         StringBuilder content = new StringBuilder();
         while ((inputLine = in.readLine()) != null) {
@@ -76,12 +98,70 @@ public class Main {
         in.close();
     }
 
+    private static Document read(HttpURLConnection con) throws IOException {
+        BufferedReader in = new BufferedReader(
+                new InputStreamReader(con.getInputStream()));
+        String inputLine;
+        StringBuilder content = new StringBuilder();
+        while ((inputLine = in.readLine()) != null) {
+            content.append(inputLine);
+        }
+        Document doc = Jsoup.parse(content.toString());
+        in.close();
+        return doc;
+    }
+
+    private static LocalDate checkMostRecentDate() throws IOException {
+        Document basePage = read(getFbBaseConnection());
+        String zf = getZF(basePage);
+        Document bookingPage = read(getFbConnection(zf));
+        String month = getFirstAvailableMonth(bookingPage);
+        String year = "2023";
+        String dayOfMonth = getFirstDay(bookingPage);
+
+        LocalDate date = LocalDate.of(parseInt(year), Month.getMonthId(month), parseInt(dayOfMonth.trim()));
+        return date;
+    }
+
     private static HttpURLConnection getConnection(int officeId) throws IOException {
-        URL url = new URL(APPOINTMENT_URL + officeId + STEP_PARAM);
+        URL url = new URL(APPOINTMENT_BÜRGERBÜRO_URL + officeId + STEP_PARAM);
         HttpURLConnection con = (HttpURLConnection) url.openConnection();
         con.setRequestMethod("POST");
         con.setRequestProperty("Content-Type", CONTENT_TYPE);
         return con;
+    }
+
+    private static HttpURLConnection getFbBaseConnection() throws IOException {
+        URL url = new URL(APPOINTMENT_FÜHRERSCHEINBEHÖRDE_URL);
+        HttpURLConnection con = (HttpURLConnection) url.openConnection();
+        con.setRequestMethod("POST");
+        con.setRequestProperty("Content-Type", CONTENT_TYPE);
+        return con;
+    }
+
+    private static HttpURLConnection getFbConnection(String zf) throws IOException {
+        String FORM_DATA="casetype_104=1&sentcasetypes=Weiter";
+        URL url = new URL(APPOINTMENT_FÜHRERSCHEINBEHÖRDE_URL + STEP_PARAM + DIENSTLEISTUNG_PARAM + zf + FORM_DATA);
+        HttpURLConnection con = (HttpURLConnection) url.openConnection();
+        con.setRequestMethod("POST");
+        con.setRequestProperty("Content-Type", CONTENT_TYPE);
+        con.setDoOutput(true);
+        try( DataOutputStream wr = new DataOutputStream( con.getOutputStream())) {
+            wr.write( FORM_DATA.getBytes(StandardCharsets.UTF_8) );
+        }
+//        String urlParameters = "Content-Disposition: form-data; casetype_104=1; sentcasetypes=Weiter\r\n";
+//        DataOutputStream wr = new DataOutputStream(con.getOutputStream());
+//        wr.writeBytes(urlParameters);
+//        wr.flush();
+//        wr.close();
+        return con;
+    }
+
+    private static String getZF(Document doc) {
+        return doc.select("div.terminbuchung")
+                .select("form")
+                .attr("action")
+                .split("zf=")[1];
     }
 
     private static String getFirstAvailableMonth(Document doc) {
--- src/main/java/utils/Constants.java
+++ src/main/java/utils/Constants.java
@@ -5,8 +5,10 @@ public class Constants {
     public static final String EMAIL_TO = "email_to";
     public static final String FROM_PASSWORD = "from_password";
 
-    public static final String APPOINTMENT_URL = "https://termine.dresden.de/netappoint/index.php?company=stadtdresden-bb&cur_cause=";
+    public static final String APPOINTMENT_BÜRGERBÜRO_URL = "https://termine.dresden.de/netappoint/index.php?company=stadtdresden-bb&cur_cause=";
+    public static final String APPOINTMENT_FÜHRERSCHEINBEHÖRDE_URL = "https://termine.dresden.de/netappoint/index.php?company=stadtdresden-fs";
     public static final String STEP_PARAM = "&step=2";
+    public static final String DIENSTLEISTUNG_PARAM = "&zf=";
 
     public static final String CONTENT_TYPE = "application/x-www-form-urlencoded";
 }
